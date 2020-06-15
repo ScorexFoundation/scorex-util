924a08343f4b64677a5996c7ba61c2813b469a47
Konstantin Knizhnik <knizhnik@garret.ru>
2020-06-15 10:04:44 +0300

    [refer #1132] Change ModifierId representation

--- src/main/scala/scorex/util/package.scala
+++ src/main/scala/scorex/util/package.scala
@@ -5,12 +5,39 @@ import supertagged.TaggedType
 
 package object util {
 
-  object ModifierId extends TaggedType[String]
-  type ModifierId = ModifierId.Type
+  case class ModifierId(hashBytes: Array[Byte]) {
+    // This is much more efficient than hashing whole array or String.
+    // We can use the first 4 bytes and convert them into Int.
+    override def hashCode: Int = {
+      val len = math.min(hashBytes.length, 4)
+	  var hash = 0
+	  for (i <- 0 until len) {
+	    hash = (hash << 8) | (hashBytes(i) & 0xFF)
+	  }
+	  hash
+	}
+    override def equals(other: Any): Boolean = other.isInstanceOf[ModifierId] && hashBytes.sameElements(other.asInstanceOf[ModifierId].hashBytes)
+	override def toString: String = Base16.encode(hashBytes)
+  }
 
-  def bytesToId(bytes: Array[Byte]): ModifierId = ModifierId @@ Base16.encode(bytes)
+  def bytesToId(bytes: Array[Byte]): ModifierId = new ModifierId(bytes)
 
-  def idToBytes(id: ModifierId): Array[Byte] = Base16.decode(id).get
+  def idToBytes(id: ModifierId): Array[Byte] = id.hashBytes
+
+  def stringToId(s: String): ModifierId = new ModifierId(Base16.decode(s).get)
+
+  implicit val modifierOrdering : Ordering[ModifierId] = new Ordering[ModifierId] {
+    def compare(a: ModifierId, b: ModifierId): Int = {
+      val len = math.min(a.hashBytes.length, b.hashBytes.length)
+	  for (i <- 0 until len) {
+	    val diff = (a.hashBytes(i) & 0xFF) - (b.hashBytes(i) & 0xFF)
+        if (diff != 0) {
+		  return diff
+		}
+      }
+	  return a.hashBytes.length - len
+	}
+  }
 
   implicit class ModifierIdOps(val m: ModifierId) extends AnyVal {
     @inline def toBytes: Array[Byte] = idToBytes(m)
@@ -18,5 +45,13 @@ package object util {
 
   implicit class ByteArrayOps(val b: Array[Byte]) extends AnyVal  {
     @inline def toModifierId: ModifierId = bytesToId(b)
+  }
+
+  implicit class StringOps(val s: String) extends AnyVal  {
+    @inline def toModifierId: ModifierId = stringToId(s)
+  }
+
+  object ModifierId {
+    def apply(s: String): ModifierId = stringToId(s)
   }
 }
